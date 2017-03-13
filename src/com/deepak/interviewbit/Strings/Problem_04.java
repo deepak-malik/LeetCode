44516cc5b77291da175f9939aba740f50c65dfcb
Deepak <d_malik@apple.com>
2017-03-13 16:07:49 -0700

    Strings - Problem 04

--- src/com/deepak/interviewbit/Strings/Problem_04.java
+++ src/com/deepak/interviewbit/Strings/Problem_04.java
@@ -7,7 +7,8 @@ package com.deepak.interviewbit.Strings;
 /**
  * <br> Problem Statement :
  * 
- * Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+ * Given a string s consists of upper/lower-case alphabets and empty space characters ' ', 
+ * return the length of last word in the string.
  * If the last word does not exist, return 0.
  * Note: A word is defined as a character sequence consists of non-space characters only.
  * 

9e63f8a67313e86129751f2851e1db103eee0e8f
Deepak Malik <deepak-malik@users.noreply.github.com>
2017-03-29 10:58:11 -0700

    Update Problem_03.java

--- src/com/deepak/leetcode/Trees/Problem_03.java
+++ src/com/deepak/leetcode/Trees/Problem_03.java
@@ -16,9 +16,9 @@ import com.deepak.leetcode.Library.TreeNode;
  * Example:
  * 			 3
  * 			/ \
- * 		   9  20
- * 			 /  \
- * 			15   7
+ * 		       9  20
+ * 			  /  \
+ * 			 15   7
  * 
  * There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
  * 
